1fe62026da0083d8e40d04168a99044057d5532a
Mark deVilliers <markdevilliers@gmail.com>
2019-11-07 15:28:28 +0000

    :

---
+++ exec/exec.go
@@ -0,0 +1,39 @@
+package exec
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// GetCommandOutput evaluates the given command and returns the trimmed output
+func GetCommandOutput(dir string, name string, args ...string) (string, error) {
+	e := exec.Command(name, args...)
+	if dir != "" {
+		e.Dir = dir
+	}
+	data, err := e.CombinedOutput()
+	text := string(data)
+	text = strings.TrimSpace(text)
+	return text, err
+}
+
+// RunCommand evaluates the given command an error if any
+func RunCommand(dir string, name string, args ...string) error {
+	e := exec.Command(name, args...)
+	if dir != "" {
+		e.Dir = dir
+	}
+	e.Stdout = os.Stdout
+	e.Stderr = os.Stdin
+	return e.Run()
+}
+
+// RunCommandSilent evaluates the given command throwing away any output
+func RunCommandSilent(dir string, name string, args ...string) error {
+	e := exec.Command(name, args...)
+	if dir != "" {
+		e.Dir = dir
+	}
+	return e.Run()
+}
---
+++ exec/go.mod
@@ -0,0 +1,3 @@
+module github.com/mdevilliers/go/exec
+
+go 1.13
